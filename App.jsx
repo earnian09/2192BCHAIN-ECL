2ea56a641db79f69795eca2882b353b9ab6d3b63
earnian09 <81209243+earnian09@users.noreply.github.com>
2024-01-09 14:39:15 +0800

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import ParentComponent from './components/ParentComponent'
+
+function App() {
+  
+  return(
+    <>
+      <div>
+        <h1>React Props Example</h1>
+        <ParentComponent />
+      </div>
+    </>
+  )
+}
+
+export default App

35239ed7519b2f2aadb95b69fa204584c6555657
earnian09 <81209243+earnian09@users.noreply.github.com>
2024-01-09 14:44:32 +0800

    Add files via upload

---
+++ ParentComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ChildComponent from './ChildComponent';
+
+function ParentComponent() {
+    const dataToPass = "Hello from parent!";
+    return (
+        <ChildComponent message={dataToPass} />
+    );
+}
+
+export default ParentComponent;
